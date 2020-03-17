420dd02e0e0161c2c4223db9f4b94f094b5134c1
tree-project <62279579+tree-project@users.noreply.github.com>
2020-03-17 12:21:50 +0200

    message
    Signed-off-by: tree-project <62279579+tree-project@users.noreply.github.com>

--- src/TP/MainProject/DataListViewer.java
+++ src/TP/MainProject/DataListViewer.java
@@ -1,39 +1,4 @@
 package TP.MainProject;
 
-import TP.Resources;
-import TP.Tree;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 public class DataListViewer {
-    //TODO searcher and viewer
-    static Tree[] trees = Resources.trees;
-    static File file = new File("C:\\Users\\Sharfi\\Documents\\TreeProject\\TreeData.txt");
-    static FileOutputStream fos;
-
-    static {
-        try {
-            fos = new FileOutputStream(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    } // initialize fos
-
-    public static void SaveTreeData(String toWrite) throws IOException {
-        if (!file.exists()) {
-            file.createNewFile();
-        }
-
-        char[] toWriteEncoding = new char[toWrite.length()];
-        byte[] toWriteEncoded = new byte[toWrite.length()];
-        for (int i = 0; i < toWrite.length() ; i++) {
-            toWriteEncoding[i] = toWrite.charAt(i);
-            toWriteEncoded[i] = (byte)toWriteEncoding[i];
-        }
-
-        fos.write(toWriteEncoded);
-    }
 }
