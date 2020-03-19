af386c57bc2b302863f34a5d8f5495bf8bdc9fda
tree-project <62279579+tree-project@users.noreply.github.com>
2020-03-19 19:08:23 +0200

    message
    Signed-off-by: tree-project <62279579+tree-project@users.noreply.github.com>

--- src/TP/Tree.java
+++ src/TP/Tree.java
@@ -1,13 +1,25 @@
 package TP;
 
+import java.io.File;
+
 public class Tree {
     private final String lastWatered;
     private final String lastFertiized;
-    private final String growth;
+    private final int growth;
 
-    public Tree(String lastWatered1, String lastFertilized1, String growth1) {
+    public Tree(String lastWatered1, String lastFertilized1, int growth1) {
         this.lastWatered = lastWatered1;
         this.lastFertiized = lastFertilized1;
         this.growth = growth1;
+    }
+
+    public Tree initTreesScan (String filePath, int amount) {
+        Tree tree = new Tree("2" ,"2", 2);
+        //TODO tree scanner from file
+        File treeFile = new File(filePath);
+        for (int i = 0; i <= amount; i++) {
+
+        }
+        return tree;
     }
 }
